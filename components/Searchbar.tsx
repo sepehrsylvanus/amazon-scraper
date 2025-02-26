5a33a8ed44791dae127d1cbf08a02b0842b9d691
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-02-26 19:24:16 +0300

    homepage

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,8 +1,42 @@
 "use client";
-import React from "react";
+import React, { FormEvent, useState } from "react";
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon") ||
+      hostname.endsWith("amazon")
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+  return false;
+};
 
 const Searchbar = () => {
-  const handleSubmit = () => {};
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const isvalidLink = isValidAmazonProductURL(searchPrompt);
+    if (!isvalidLink) {
+      return alert("Please enter a valid Amazon product link");
+    }
+
+    try {
+      setLoading(true);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
@@ -10,10 +44,16 @@ const Searchbar = () => {
         type="text"
         placeholder="Enter product link"
         className="searchbar-input"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
       />
 
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ""}
+      >
+        {loading ? "Searching... " : "Search"}
       </button>
     </form>
   );
---
+++ lib/scraper/index.ts
@@ -0,0 +1,6 @@
+export async function scrapeAmazonProduct(url: string) {
+  if (!url) return;
+
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+}
