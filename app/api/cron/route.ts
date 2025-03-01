6e6726159d5059cfc17c8ca4ee7faab97f44310b
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-03-01 12:28:28 +0300

    add settings to API

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,6 +11,9 @@ import {
 import { error } from "console";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300;
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export async function GET() {
   try {
     connectToDB();
